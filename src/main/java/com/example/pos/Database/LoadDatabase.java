d06b79db7c4e22b7dafe979bcbbff3405b7ac2f2
kamile171 <76160527+kamile171@users.noreply.github.com>
2023-01-03 00:01:08 +0200

    add entries to db

--- src/main/java/com/example/pos/Database/LoadDatabase.java
+++ src/main/java/com/example/pos/Database/LoadDatabase.java
@@ -2,9 +2,11 @@ package com.example.pos.Database;
 
 import com.example.pos.Models.CashRegister;
 import com.example.pos.Models.Item;
+import com.example.pos.Models.Order;
 import com.example.pos.Models.Subscription;
 import com.example.pos.Repositories.Interface.CashRegisterRepository;
 import com.example.pos.Repositories.Interface.ItemRepository;
+import com.example.pos.Repositories.Interface.OrderRepository;
 import com.example.pos.Repositories.Interface.SubscriptionRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,6 +15,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 
 @Configuration
@@ -20,7 +23,7 @@ class LoadDatabase {
     private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
 
     @Bean
-    CommandLineRunner initDatabase(ItemRepository itemRepository, SubscriptionRepository subscriptionRepository, CashRegisterRepository cashRegisterRepository) {
+    CommandLineRunner initDatabase(ItemRepository itemRepository, SubscriptionRepository subscriptionRepository, CashRegisterRepository cashRegisterRepository, OrderRepository orderRepository) {
 
         return args -> {
             //items
@@ -33,6 +36,10 @@ class LoadDatabase {
             log.info("Preloading " + cashRegisterRepository.save(new CashRegister(Long.valueOf(1), Long.valueOf(1), new BigDecimal("1255"), 54, new BigDecimal("500"), "Eur" )));
             log.info("Preloading " + cashRegisterRepository.save(new CashRegister(Long.valueOf(1), Long.valueOf(1),new BigDecimal("25"), 7, new BigDecimal("8"),"Eur" )));
             log.info("Preloading " + cashRegisterRepository.save(new CashRegister(Long.valueOf(2), Long.valueOf(1),new BigDecimal("3"), 2, new BigDecimal("1"), "Eur")));
+            //order
+            log.info("Preloading " + orderRepository.save(new Order(Long.valueOf(1), Long.valueOf(1),12.22, LocalDateTime.now(), "cash", true, 16.0, "some comment", new Long[]{Long.valueOf(1),Long.valueOf(2),Long.valueOf(2)})));
+            log.info("Preloading " + orderRepository.save(new Order(Long.valueOf(1), Long.valueOf(1),52.22, LocalDateTime.now(), "cash", true, 20.0, "another comment", new Long[]{Long.valueOf(1),Long.valueOf(1),Long.valueOf(2)})));
+
         };
     }
 
